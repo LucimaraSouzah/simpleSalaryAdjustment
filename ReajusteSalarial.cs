16e45d67bfdd24814af6333ca4d01a6ca05d3be3
Lucimara Souza <lucimarasouzah@gmail.com>
2022-11-24 16:00:49 -0300

    Calculo de reajuste salarial

---
+++ ReajusteSalarial.cs
@@ -0,0 +1,11 @@
+﻿using System;
+public class ReajusteSalarial
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Digite o valor do salário: ");
+        double salario = double.Parse(Console.ReadLine());
+        salario = salario + (salario * 0.1);
+        Console.WriteLine("Com o reajuste, o salário será: " + salario);
+    }
+}
